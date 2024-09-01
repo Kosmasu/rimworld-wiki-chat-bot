bb5e0d71f16324827b099f540633ad70104ae64e
Kenny <kennylink2626@gmail.com>
2024-09-02 00:19:53 +0800

    very simple chat ui, and very rough rimbot

--- src/settings.py
+++ src/settings.py
@@ -2,7 +2,14 @@
 
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DB_CONN_STRING = os.getenv("DB_CONN_STRING", "")
 MAIN_PAGE_URL = "https://rimworldwiki.com/"
 SEARCH_PAGE_URL = "https://rimworldwiki.com/index.php"
+
+DEEP_INFRA_API_KEY=os.getenv("DEEP_INFRA_API_KEY", "")
+if DEEP_INFRA_API_KEY == "":
+    raise Exception("DEEP_INFRA_API_KEY is not set!")
